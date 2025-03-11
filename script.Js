67175cd78a4f6782e4138b044bc32fa42c09644d
GoldXdevfoundation <ashiddiqynickola@gmail.com>
2025-03-11 19:11:36 +0700

    Delete script.Js

--- script.Js
+++
@@ -1,7 +0,0 @@
-let cart = [];
-
-function addToCart(product, price) {
-    cart.push({ product, price });
-    alert(`${product} ditambahkan ke keranjang!`);
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
